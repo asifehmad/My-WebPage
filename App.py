3f3004ee25cba50892421cdcd808dc9a3fb8bfc0
Asif Ahmad <54246513+asifehmed@users.noreply.github.com>
2021-08-09 09:49:50 +0500

    Add files via upload

---
+++ App.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+header = st.container()
+qualification = st.container()
+designation = st.container()
+publications = st.container()
+
+with header:
+    st.title('Welcome to Asif Ahmad WebPage')
+    st.text('Hello, all! This Asif Ahmad Master of Science in Mathematics')
+
+with qualification:
+    st.title('Qualification')
+    st.text('Master of Science in Mathematics')
+
+with designation:
+    st.title('Designation')
+    st.text('Lecturer in Mathematics')
+
+with publications:
+    st.title('Publications')
+    st.text('Two Conference paper')
+
